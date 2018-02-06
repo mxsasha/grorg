4f348c2b32cf1f23577499d6a5f142b85bfd35ef
Sasha Romijn <github@mxsasha.eu>
2018-02-06 15:15:40 +0100

    Fixed migrations

---
+++ grants/migrations/0015_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('grants', '0014_auto_20180103_1621'),
+        ('grants', '0013_auto_20180103_1447'),
+    ]
+
+    operations = [
+    ]
